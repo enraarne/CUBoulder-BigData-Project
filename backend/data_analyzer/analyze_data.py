52905f0e2c4e932f3a7ce9ac89efe2dedb3864c8
enraarne <arne.fevolden@yahoo.com>
2023-08-04 08:40:49 +0200

    make analyzer stand alone

--- backend/data_analyzer/analyze_data.py
+++ backend/data_analyzer/analyze_data.py
@@ -2,12 +2,26 @@ import pandas as pd
 from tinydb import TinyDB, Query
 from backend.data_analyzer.weather_data_categories import transformation_dict
 
-def analyzer(x_varnish):
-    
-    db = TinyDB('backend/weather_db')
-    User = Query()
+def analyzer(data, stand_alone_program=False):
+    """
+    To use the analyzer as a stand alone program, set stand_alone_program to True. The data must be provided
+    as a dictionary with the following format:
 
-    result = db.search(User.x_varnish == x_varnish)[0]
+    data = {
+    "dates": ["07-28-2023", "07-29-2023", "07-30-2023", "07-31-2023", "08-01-2023", "08-02-2023", "08-03-2023"], 
+    "weather": ["cloudy", "rainshowers_day", "fair_day", "fair_night", "cloudy", "partlycloudy_day", "partlycloudy_day", "cloudy", "cloudy", "rain", "rain", "cloudy", "cloudy", "rain", "rain", "cloudy", "rain", "cloudy", "rain", "cloudy", "cloudy", "rainshowers_day", "rain", "cloudy", "partlycloudy_night", "cloudy", "cloudy", "partlycloudy_night"]
+    }
+
+    Otherwise, the program will use the the data as x_varnish values for fetching information from the database 
+    """
+
+    if stand_alone_program == False:
+        db = TinyDB('backend/weather_db')
+        User = Query()
+
+        result = db.search(User.x_varnish == data)[0]
+    else:
+        result = data
 
     # producing dates for the frontend
     dates = result['dates']
